f3c15591bced45181154f090b0ceceb50819cda5
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-08-22 23:48:59 +0530

    Project Structure + Model Class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,11 @@ dependencies {
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // GSON Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
 
 kapt {
