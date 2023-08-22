da75ca2923ca2ab06e5e9fbf9f7519216ae3fc1d
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-08-23 00:17:22 +0530

    Retrofit Instance with DI

---
+++ app/src/main/java/com/example/jetquiz/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.jetquiz.di
+
+import com.example.jetquiz.network.QuestionsAPI
+import com.example.jetquiz.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionAPI(): QuestionsAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionsAPI::class.java)
+    }
+
+}
